c165b33e47ca4d8e2e1bf06d7cc21d1fcddf4824
Hans Moleman <aceind13@aol.com>
2025-10-28 22:16:35 -0230

    Implemented ability to write CPU usage data to file as it is monitored.

--- src/main.py
+++ src/main.py
@@ -19,11 +19,22 @@ import time
 
 if __name__ == "__main__":
 	wait_time = 0.5
+	data_path = "data.csv"
+	elapsed = 0
+	#fobj = None
 
 	try:
+		fobj = open(data_path, 'a')
+		wstr = ""
+
 		while True:
 			cpu_usage = psutil.cpu_percent(interval=None)
-			print(f"CPU:  {cpu_usage}")
+			elapsed += wait_time
+			wstr = f"{elapsed}, {cpu_usage}\n"
+			fobj.write(wstr)
+			print(f"CPU@{elapsed}:  {cpu_usage}")
 			time.sleep(wait_time)
 	except:
 		print()
+	finally:
+		fobj.close()
