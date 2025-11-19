b08c644232749e53635a97878bd7c0bd1e77f1d6
Hans Moleman <aceind13@aol.com>
2025-11-19 19:47:28 -0330

    Modified main to check for a pre-existing data file from a previous session and remove it automatically, then implemented ability to fetch (percent) usage of RAM and write it to file along with CPU (percent) usage. System is tested working.

--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@
 # ---
 ##
 
+import os
 import psutil
 import time
 
@@ -24,15 +25,22 @@ if __name__ == "__main__":
 	#fobj = None
 
 	try:
+		home_path = os.path.dirname(os.path.abspath(__file__))
+		if os.path.isfile(f"{home_path}/{data_path}"):
+			os.remove(f"{home_path}/{data_path}")
 		fobj = open(data_path, 'a')
 		wstr = ""
 
 		while True:
 			cpu_usage = psutil.cpu_percent(interval=None)
+			ram_usage = psutil.virtual_memory().percent
 			elapsed += wait_time
-			wstr = f"{elapsed}, {cpu_usage}\n"
+
+			wstr = f"{elapsed}, {cpu_usage}, {ram_usage}\n"
 			fobj.write(wstr)
+			print()
 			print(f"CPU@{elapsed}:  {cpu_usage}")
+			print(f"RAM@{elapsed}:  {ram_usage}")
 			time.sleep(wait_time)
 	except:
 		print()
